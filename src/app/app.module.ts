b5006483d60278479c3595e6a954353266e62780
lendilai <ngethenan768@gmail.com>
2019-08-21 15:38:25 +0300

    Import the neccessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { HttpClientModule } from '@angular/common/http';
+
+const firebaseConfig = {
+ apiKey: "AIzaSyAvYCgR9Kz3ACF91YfQL-vV-mN7JPYD2B4",
+ authDomain: "testing-5c7bc.firebaseapp.com",
+ databaseURL: "https://testing-5c7bc.firebaseio.com",
+ projectId: "testing-5c7bc",
+ storageBucket: "",
+ messagingSenderId: "650436830549",
+ appId: "1:650436830549:web:6ec5277687954807"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +25,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
