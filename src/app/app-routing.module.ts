2b8f7d7fcd464a4d06ad35e0d0c0b49f5c138d2f
lendilai <ngethenan768@gmail.com>
2019-08-22 16:00:06 +0300

    Change google sign in method to implement the general login method

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {path:'', redirectTo:'/sign-in', pathMatch:'full'},
   {path:'forgotPassword', component:ForgotPasswordComponent},
-  {path:'home', component:HomeComponent},
+  {path:'home', component:HomeComponent, canActivate:[AuthGuard]},
   {path:'sign-in', component:SignInComponent},
   {path:'createAccount', component:SignUpComponent},
   {path:'verify', component:VerifyEmailComponent},
