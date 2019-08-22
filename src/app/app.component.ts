62d8e604e5e6d3025495de01754b69aacd64bf41
MarkMwago2 <markmwago@gmail.com>
2019-08-22 17:26:30 +0300

    create location request popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sift';
+
+     ngOninit(){
+    //function that gets the location and returns it
+    function getLocation() {
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        console.log("Geo Location not supported by browser");
+      }
+    }
+    //function that retrieves the position
+    function showPosition(position) {
+      var location = {
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude
+      }
+      console.log(location)
+    }
+    //request for location
+    getLocation();
+
+
 }
+    }
