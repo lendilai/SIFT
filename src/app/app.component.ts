309603c187ee2bca5fded66e5e9723d247498da2
MarkMwago2 <markmwago@gmail.com>
2019-08-26 09:31:48 +0300

    add Google location API keys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,4 @@ import { OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sift';
-
-     ngOninit(){
-    //function that gets the location and returns it
-    function getLocation() {
-      if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-        console.log("Geo Location not supported by browser");
-      }
-    }
-    //function that retrieves the position
-    function showPosition(position) {
-      var location = {
-        longitude: position.coords.longitude,
-        latitude: position.coords.latitude
-      }
-      console.log(location)
-    }
-    //request for location
-    getLocation();
-
-
-}
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core'
 
 @NgModule({
   declarations: [
@@ -11,6 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAJXMzzE17saSH55EZCT2m1YLsfaEWfNsY',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
