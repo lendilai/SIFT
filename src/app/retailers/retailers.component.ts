e96746deaf7d9cbd22b0ce19cc3c61670f273653
lendilai <ngethenan768@gmail.com>
2019-08-29 05:00:04 +0300

    Add ng5-slider module to the app module

--- src/app/retailers/retailers.component.ts
+++ src/app/retailers/retailers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Options, LabelType } from 'ng5-slider';
 
 @Component({
   selector: 'app-retailers',
@@ -8,6 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class RetailersComponent implements OnInit {
 
   constructor() { }
+  private minPrice: number;
+  private maxPrice: number;
+  private modalIsVisible:boolean = true;
+  private ratingVisible:boolean = false;
+  private ratingValue;
+  minValue:number = 500;
+  maxValue: number = 30000;
+  options:Options = {
+    floor: 0,
+    ceil: 50000,
+    animate:false,
+    translate: (value:number, label:LabelType): string => {
+      switch(label){
+        case LabelType.Low:
+          this.minPrice = value;
+          return "<b></b>" + value;
+        case LabelType.High:
+          this.maxPrice = value;
+            return "<b></b>" + value;
+            default:
+              return "Kes"+ value;
+      }
+    }
+  };
+  toggle(){
+    this.modalIsVisible  = !this.modalIsVisible;
+    this.ratingVisible = false;
+  }
+
+  toggleRating(){
+    this.ratingVisible = !this.ratingVisible;
+    this.modalIsVisible = false;
+  }
+
 
   ngOnInit() {
   }
