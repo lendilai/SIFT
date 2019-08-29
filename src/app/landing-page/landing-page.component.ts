60e3395108a4e81d8da44030bb80be84145cfad0
gkarumba <gachegua@gmail.com>
2019-08-29 10:33:29 +0300

    Add the search functionality to the landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,8 +10,23 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  public searchterm: string;
+  public products: string;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private prod: ProductService, private router: Router) { }
+
+  searchProduct() {
+    this.prod.searchProduct(this.searchterm).subscribe(res => {
+      this.products = JSON.stringify(res);
+      console.log(this.products);
+      localStorage.setItem('products', this.products);
+      this.router.navigate(['/retailers']);
+    },
+    err => {
+      console.log(err);
+      alert('Could not find the item you\'re looking for');
+    });
+  }
 
   ngOnInit() {
   }
